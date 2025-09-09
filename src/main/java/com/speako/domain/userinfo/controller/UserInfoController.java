58c42682e059380cae7f0ae1af3770dc94f01cbe
leesj <jinycap7@gmail.com>
2025-09-10 02:27:29 +0900

    [Feat] 타인 마이페이지 조회 기능 추가 #88

--- src/main/java/com/speako/domain/userinfo/controller/UserInfoController.java
+++ src/main/java/com/speako/domain/userinfo/controller/UserInfoController.java
@@ -3,7 +3,7 @@ package com.speako.domain.userinfo.controller;
 import com.speako.domain.auth.annotation.LoginUser;
 import com.speako.domain.security.principal.CustomUserDetails;
 import com.speako.domain.userinfo.dto.reqDTO.UpdateSelfCommentReqDTO;
-import com.speako.domain.userinfo.dto.resDTO.UserInfoResDTO;
+import com.speako.domain.userinfo.dto.resDTO.MyPageResDTO;
 import com.speako.domain.userinfo.dto.resDTO.userAchievement.UpdateSelfCommentResDTO;
 import com.speako.domain.userinfo.service.command.UserInfoCommandService;
 import com.speako.domain.userinfo.service.query.UserInfoQueryService;
@@ -15,25 +15,28 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users-info")
+@RequestMapping("/api/users")
 public class UserInfoController {
 
     private final UserInfoCommandService userInfoCommandService;
     private final UserInfoQueryService userInfoQueryService;
 
-    @GetMapping("/mypage")
-    @Operation(method = "GET", summary = "사용자의 마이페이지 전체 정보 조회 API", description = "현재 유저의 마이페이지 정보(사용자 전체 통계+월별통계)를 조회하는 API입니다.")
-    public CustomResponse<UserInfoResDTO> getUserInfo(
-            @LoginUser CustomUserDetails userDetails) {
+    @GetMapping("/{userId}/mypage")
+    @Operation(method = "GET", summary = "마이페이지 정보 조회 API",
+            description = "본인 또는 타인의 마이페이지 정보를 조회하는 API입니다.\n\n"
+                    + "**세부 설명:**\n"
+                    + "- `PathVariable 'userId'(targetUserId)`: 조회하려는 마이페이지의 소유자 id\n\n"
+                    + "- `본인의 마이페이지를 조회할 경우`(targetUserId가 로그인된 사용자의 userId와 같을 경우)\n\n"
+                    + "    - [userId / 유저 대표정보 / 월별통계]만 반환하며, 획득 뱃지리스트는 제외(null)됩니다.\n\n"
+                    + "- `타인의 마이페이지를 조회할 경우`(targetUserId가 로그인된 사용자의 userId와 다를 경우)\n\n"
+                    + "    - [userId / 유저 대표정보 / 획득 뱃지 리스트]만 반환하며, 월별통계는 제외(null)됩니다.")
+    public CustomResponse<MyPageResDTO> getMyPage(
+            @LoginUser CustomUserDetails userDetails,
+            @PathVariable(value = "userId") Long targetUserId) {
 
-        UserInfoResDTO userInfoResDTO = userInfoQueryService.getUserInfo(userDetails.getId());
-        return CustomResponse.onSuccess(userInfoResDTO);
+        MyPageResDTO myPageResDTO = userInfoQueryService.getMyPage(userDetails.getId(), targetUserId);
+        return CustomResponse.onSuccess(myPageResDTO);
     }
-
-    /* 유저 대표정보만 get하는 API 구현 필요
-        - 타인의 마이페이지 조회 시 사용 예정 (userBadge 조회 API랑 각각 실행되거나, userBadge도 같이 조회하거나 둘 중 하나)
-        - @LoginUser와 userId 둘 다 받아와서, 비교해서 같으면 진짜 내 페이지, 다르면 상대페이지 조회되도록 구현하는 방법도 존재
-     */
 
     @PatchMapping("/self-comment")
     @Operation(method = "PATCH", summary = "한줄소개 update API", description = "사용자의 한줄소개를 변경하는 API입니다.")
--- src/main/java/com/speako/domain/userinfo/service/query/UserInfoQueryService.java
+++ src/main/java/com/speako/domain/userinfo/service/query/UserInfoQueryService.java
@@ -1,11 +1,14 @@
 package com.speako.domain.userinfo.service.query;
 
+import com.speako.domain.challenge.converter.UserBadgeConverter;
+import com.speako.domain.challenge.dto.UserBadgeResponse;
+import com.speako.domain.challenge.service.query.UserChallengeQueryService;
 import com.speako.domain.user.domain.User;
 import com.speako.domain.user.exception.UserErrorCode;
 import com.speako.domain.user.repository.UserRepository;
 import com.speako.domain.userinfo.dto.resDTO.monthlyStat.MonthlyStatResDTO;
 import com.speako.domain.userinfo.dto.resDTO.userAchievement.UserAchievementResDTO;
-import com.speako.domain.userinfo.dto.resDTO.UserInfoResDTO;
+import com.speako.domain.userinfo.dto.resDTO.MyPageResDTO;
 import com.speako.domain.userinfo.service.query.monthlyStat.MonthlyStatQueryService;
 import com.speako.domain.userinfo.service.query.userAchievement.UserAchievementQueryService;
 import com.speako.global.apiPayload.exception.CustomException;
@@ -24,16 +27,30 @@ public class UserInfoQueryService {
 
     private final UserAchievementQueryService userAchievementService;
     private final MonthlyStatQueryService monthlyStatService;
+    private final UserChallengeQueryService userChallengeQueryService;
 
-    // 사용자의 마이페이지 정보(유저 대표정보+월별통계)를 조회
-    public UserInfoResDTO getUserInfo(Long userId) {
-
-        User user = userRepository.findById(userId)
+    // 본인 또는 타인의 마이페이지 정보 조회 API
+    public MyPageResDTO getMyPage(Long userId, Long targetUserId) {
+        /*
+            로직 상단에서 findById(targetUserId) 하는 이유
+            1. targetUserId의 유효성 판단 (존재하는 userId인가?)
+            2. 분기 시에도 공통으로 사용하기 위함 (본인이든 타인이든, 조회할 마이페이지는 targetUser 것이기 때문)
+        */
+        User targetUser = userRepository.findById(targetUserId)
                 .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
-        // 유저 대표정보와 월별통계를 조회한 후 UserInfoResDTO를 생성하여 반환
-        UserAchievementResDTO achievement = userAchievementService.getUserAchievement(user);
-        List<MonthlyStatResDTO> monthlyStats = monthlyStatService.get6RecentMonthlyStats(user.getId());
+        // 두 경우에서 공통으로 조회되는 UserAchievement 미리 조회
+        UserAchievementResDTO achievement = userAchievementService.getUserAchievement(targetUser);
 
-        return new UserInfoResDTO(userId, achievement, monthlyStats);
+        if (userId.equals(targetUserId)) {
+            // 본인의 마이페이지 조회 (유저 대표정보와 월별통계 조회)
+            List<MonthlyStatResDTO> monthlyStats = monthlyStatService.get6RecentMonthlyStats(targetUser.getId());
+            return new MyPageResDTO(true, targetUserId, achievement, monthlyStats, null);
+        } else {
+            // 타인의 마이페이지 조회 (유저 대표정보와 획득 뱃지리스트 조회)
+            List<UserBadgeResponse> userBadges = userChallengeQueryService.getUserBadges(targetUserId)
+                    .stream().map(UserBadgeConverter::toUserBadgeResponse)
+                    .toList();
+            return new MyPageResDTO(false, targetUserId, achievement, null, userBadges);
+        }
     }
 }
